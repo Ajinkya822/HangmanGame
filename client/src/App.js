3cd4c9cf72d92b5b09aa2ac72aedf9759494ae51
ajinkyausc <nagare@usc.edu>
2024-05-24 14:37:53 -0500

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+  const [apiData, setApiData]=useState([{}])
+
+  useEffect(()=>{
+    fetch("/api").then(
+      response=>response.json()
+    ).then(
+      data=> {
+        setApiData(data)
+      }
+    )
+  },[])
+  return (
+    <div>
+      {(typeof apiData.users === 'undefined') ? (
+        <p> Loading data ....</p>
+      ) :(
+        apiData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))
+      )
+
+      }
+    
+    </div>
+  )
+}
+
+export default App
