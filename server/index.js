68f6f2b71b286454de0eeece198c415f81b390c7
ajinkyausc <nagare@usc.edu>
2024-05-24 15:37:31 -0500

    CORS added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express =require('express')
 const app=express()
 const cors = require('cors')
 
-app.use(cors())
+const corsOptions = { 
+  AccessControlAllowOrigin: '*',  
+  origin: '*',  
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' 
+}
+app.use(cors(corsOptions))
 
 app.get("/api", (req, res) => {
     res.json({"users":["user one", "user ten", "user four"]})
