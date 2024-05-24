bc42b39681e461ea8afec015d675a1299c023c66
ajinkyausc <nagare@usc.edu>
2024-05-24 15:19:32 -0500

    Update index js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express =require('express')
 const app=express()
 
-app.use("/", (req, res) => {
+app.use("/status", (req, res) => {
     res.send("Server is running")
 })
 
-// app.get("/api", (req, res) => {
-//     res.json({"users":["user one", "user ten", "user four"]})
-// })
+app.get("/api", (req, res) => {
+    res.json({"users":["user one", "user ten", "user four"]})
+})
 
 app.listen(5367, ()=>{console.log("Server is listening on port 5367")})
