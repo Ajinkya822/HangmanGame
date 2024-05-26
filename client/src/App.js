9901e8df33a710d5a38a228ee0bbe0dbe7b7f660
ajinkyausc <nagare@usc.edu>
2024-05-26 13:00:09 -0500

    Resume game functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={restore ? <Navigate replace to={"/start-game"} /> : <LandingPage />} />
+        <Route path="/" exact element={<LandingPage />} />
         <Route path="/start-game" element={<StartGame />} />
       </Routes>
     </Router>
